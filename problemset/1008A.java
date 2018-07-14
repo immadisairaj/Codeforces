8f43d214cb7bd330ea49bcbf3e13a0c31ba25db4
immadisairaj <immadirajendra.sai@gmail.com>
2018-07-14 11:09:20 +0530

    1008 problemset

---
+++ problemset/1008A.java
@@ -0,0 +1,52 @@
+import java.util.*;
+import java.lang.*;
+
+public class 1008A {
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+	    String word = scan.next();
+	    
+	    
+	    int count = 0;
+	    boolean wor = true;
+		
+		for(int i=0; i<word.length(); i++) {
+		    char c = word.charAt(i);
+		    switch(c) {
+		        case 'a': 
+		        case 'e':
+		        case 'i':
+		        case 'o':
+		        case 'u':
+		            if(count == 1 || count == 0) {
+		                count = 0;
+		            } else {
+		                wor = false;
+		            }
+		            break;
+		        case 'n':
+		            if(count == 1) {
+		                wor = false;
+		            }
+		            break;
+		        default:
+		            if(count == 1) {
+		                wor = false;
+		            }
+		            count++;
+		    } 
+		}
+		
+		if(count == 1) {
+	        wor = false;
+	    }
+		
+		if(wor)
+		    System.out.println("YES");
+	    else
+	        System.out.println("NO");
+		
+		scan.close();
+	}
+}
---
+++ problemset/1008B.java
@@ -0,0 +1,42 @@
+import java.util.*;
+import java.lang.*;
+
+public class 1008B {
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+	    int n = scan.nextInt();
+	    int temp;
+	    
+	    boolean ans = true;
+	    
+	    int[] wi = new int[n];
+	    int[] hi = new int[n];
+	    for(int i=0; i<n; i++) {
+	        wi[i] = scan.nextInt();
+	        hi[i] = scan.nextInt();
+	    }
+	    
+	    if(wi[0] > hi[0]) {
+	        temp = wi[0];
+	        wi[0] = hi[0];
+	        hi[0] = temp;
+	    }
+	    
+	    for(int i=1; i<n; i++) {
+	        if( (hi[i-1] >= hi[i]) || hi[i-1] >= wi[i] ) {
+	            if((wi[i] > hi[i] && wi[i] <= hi[i-1]) || (hi[i] > hi[i-1] && hi[i-1] >= wi[i])) {
+	                temp = wi[i];
+	                wi[i] = hi[i];
+	                hi[i] = temp;
+	            }
+	        } else {
+	            ans = false;
+	        }
+	    }
+	    
+	    if(ans) System.out.println("YES");
+	    else    System.out.println("NO");
+		scan.close();
+	}
+}
