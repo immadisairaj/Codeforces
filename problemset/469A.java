afe8e55c69f913a39d2bfb934c6f80cc1080fb73
immadisairaj <immadirajendra.sai@gmail.com>
2018-07-21 21:36:25 +0530

    469A

---
+++ problemset/469A.java
@@ -0,0 +1,35 @@
+import java.util.*;
+import java.lang.*;
+ 
+public class 469A {
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+	    int n = scan.nextInt();
+	    
+	    Set<Integer> numbers = new TreeSet<Integer>();
+	    
+	    int p = scan.nextInt();
+	    
+	    int num;
+	    
+	    while(p-- > 0) {
+	        num = scan.nextInt();
+	        numbers.add(num);
+	    }
+	    
+	    p = scan.nextInt();
+	    
+	    while(p-- > 0) {
+	        num = scan.nextInt();
+	        numbers.add(num);
+	    }
+	    
+	    if(numbers.size() == n)
+	        System.out.println("I become the guy.");
+	    else
+	        System.out.println("Oh, my keyboard!");
+	    
+		scan.close();
+	}
+}
