41882fc5cdbca77a75db94f561617deef240d3cb
immadisairaj <immadirajendra.sai@gmail.com>
2018-07-21 23:47:36 +0530

    554A

---
+++ problemset/554A.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+ 
+public class 554A {
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+	    String n = scan.next();
+	    
+	    System.out.println((n.length() + 1)*25 + 1);
+	    
+		scan.close();
+	}
+}
