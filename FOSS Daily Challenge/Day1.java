7850dfbee40d44fae0cf20b5c335e5079b321a86
immadisairaj <immadirajendra.sai@gmail.com>
2018-07-26 19:13:15 +0530

    Daily Challenge

---
+++ FOSS Daily Challenge/Day1.java
@@ -0,0 +1,30 @@
+import java.util.*;
+import java.lang.*;
+ 
+public class Day1 {
+	public static void main (String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+	    int n = scan.nextInt();
+	    
+	    int sum;
+	    if(n == 1)
+	        sum = 1;
+	    else if(n == 2)
+	        sum = 2;
+	    else {
+	        sum = 2;
+	        int a = 1, b = 1, c;
+	        for(int i=3; i<=n; i++) {
+	            c = a+b;
+	            sum += c;
+	            a = b;
+	            b = c;
+	        }
+	    }
+	    
+	    System.out.println(sum);
+	    
+		scan.close();
+	}
+}
