a8d3af416492e17c15a7dd7beca12b4498884c43
sureshg9573 <43848031+sureshg9573@users.noreply.github.com>
2018-10-04 16:27:28 +0530

    Update 4A.java

--- problemset/4A.java
+++ problemset/4A.java
@@ -4,7 +4,7 @@ public class 4A {
 	public static void main (String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		int n = scan.nextInt();
+		int n = scan.nextInt();  // inputs integer
 		
 		if((n==2) || (n%2!=0)) {
 		    System.out.println("NO");
